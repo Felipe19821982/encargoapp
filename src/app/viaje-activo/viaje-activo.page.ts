a61b4e8570199817a99b282b92f6689e3394f230
serbravo38 <164573839+serbravo38@users.noreply.github.com>
2024-11-12 22:10:15 -0300

    Se modifica viaje activo

--- src/app/viaje-activo/viaje-activo.page.ts
+++ src/app/viaje-activo/viaje-activo.page.ts
@@ -7,23 +7,39 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./viaje-activo.page.scss'],
 })
 export class ViajeActivoPage implements OnInit {
-  nombreUsuario: string = '';  // Inicializado con un valor por defecto
-  destino: string = '';        // Inicializado con un valor por defecto
-  fechaSalida: string = '';    // Inicializado con un valor por defecto
-  cantidadPersonas: number = 0; // Inicializado con un valor por defecto
-  costoTotal: number = 0;      // Inicializado con un valor por defecto
-  comentarios: string = '';    // Inicializado con un valor por defecto
+  nombreUsuario: string = '';
+  destinoSeleccionado: string = '';
+  fechaSalida: string = '';
+  cantidadPersonas: number = 0;
+  costoTotal: number = 0;
+  comentarios: string = '';
+  mapUrl: string = '';
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    // Suscripción a los parámetros pasados a través de la URL
     this.route.queryParams.subscribe(params => {
-      this.nombreUsuario = params['nombreUsuario'] || '';  // Usa valor por defecto si no está presente
-      this.destino = params['destino'] || '';              // Usa valor por defecto si no está presente
-      this.fechaSalida = params['fechaSalida'] || '';      // Usa valor por defecto si no está presente
-      this.cantidadPersonas = params['cantidadPersonas'] || 0; // Usa valor por defecto si no está presente
-      this.costoTotal = params['costoTotal'] || 0;         // Usa valor por defecto si no está presente
-      this.comentarios = params['comentarios'] || '';      // Usa valor por defecto si no está presente
+      this.nombreUsuario = params['nombreUsuario'] || 'Usuario';
+      this.destinoSeleccionado = params['destinoSeleccionado'] || '';
+      this.fechaSalida = params['fechaSalida'] || '';
+      this.cantidadPersonas = params['cantidadPersonas'] || 0;
+      this.costoTotal = params['costoTotal'] || 0;
+      this.comentarios = params['comentarios'] || '';
+
+      // Generar la URL del mapa según el destino
+      this.mapUrl = this.getMapUrl(this.destinoSeleccionado);
     });
+  }
+
+  // Función para generar la URL del mapa
+  getMapUrl(destino: string): string {
+    if (destino === 'puenteAlto') {
+      return 'https://maps.google.com/?q=Puente+Alto'; // Enlace a Google Maps para Puente Alto
+    } else if (destino === 'maipu') {
+      return 'https://maps.google.com/?q=Maipu'; // Enlace a Google Maps para Maipú
+    } else {
+      return ''; // Retorna una cadena vacía si no se encuentra el destino
+    }
   }
 }
