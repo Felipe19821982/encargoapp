fd5b234f1b0c8009e8f8428f799c1c50ce038ad4
serbravo38 <164573839+serbravo38@users.noreply.github.com>
2024-11-14 11:55:25 -0300

    se agrega mapa con ubicación

--- src/app/viaje-activo/viaje-activo.page.ts
+++ src/app/viaje-activo/viaje-activo.page.ts
@@ -1,24 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Geolocation } from '@capacitor/geolocation';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-viaje-activo',
   templateUrl: './viaje-activo.page.html',
   styleUrls: ['./viaje-activo.page.scss'],
 })
-export class ViajeActivoPage implements OnInit {
+export class ViajeActivoPage implements OnInit, AfterViewInit {
   nombreUsuario: string = '';
   destinoSeleccionado: string = '';
   fechaSalida: string = '';
   cantidadPersonas: number = 0;
   costoTotal: number = 0;
   comentarios: string = '';
-  mapUrl: string = '';
+  map: any;
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    // Suscripción a los parámetros pasados a través de la URL
     this.route.queryParams.subscribe(params => {
       this.nombreUsuario = params['nombreUsuario'] || 'Usuario';
       this.destinoSeleccionado = params['destinoSeleccionado'] || '';
@@ -26,20 +27,32 @@ export class ViajeActivoPage implements OnInit {
       this.cantidadPersonas = params['cantidadPersonas'] || 0;
       this.costoTotal = params['costoTotal'] || 0;
       this.comentarios = params['comentarios'] || '';
-
-      // Generar la URL del mapa según el destino
-      this.mapUrl = this.getMapUrl(this.destinoSeleccionado);
     });
   }
 
-  // Función para generar la URL del mapa
-  getMapUrl(destino: string): string {
-    if (destino === 'puenteAlto') {
-      return 'https://maps.google.com/?q=Puente+Alto'; // Enlace a Google Maps para Puente Alto
-    } else if (destino === 'maipu') {
-      return 'https://maps.google.com/?q=Maipu'; // Enlace a Google Maps para Maipú
-    } else {
-      return ''; // Retorna una cadena vacía si no se encuentra el destino
+  ngAfterViewInit() {
+    this.loadMap();
+  }
+
+  async loadMap() {
+    try {
+      const position = await Geolocation.getCurrentPosition();
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+
+      this.map = L.map('map').setView([latitude, longitude], 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+      }).addTo(this.map);
+
+      // Marcador en la ubicación actual
+      L.marker([latitude, longitude])
+        .addTo(this.map)
+        .bindPopup('Estás aquí.')
+        .openPopup();
+    } catch (error) {
+      console.error('Error al obtener la ubicación:', error);
     }
   }
 }
